4144bb0e61037479c32a14271ad4b6cb2c25192c
visavi <visavi.net@mail.ru>
2014-05-03 23:30:59 +0400

    Изменил описание

--- themes/default/views/index.php
+++ themes/default/views/index.php
@@ -15,20 +15,14 @@
 <div class="b"><img src="/images/img/lollipop.gif" alt="image" /> <b><a href="/forum">Форум</a></b> (<?=stats_forum()?>)</div>
 <?=recenttopics()?>
 
-<div class="b"><img src="/images/img/fruit-lime.gif" alt="image" /> <b><a href="/load">WAP-скрипты</a></b> (<?=stats_load()?>)</div>
+<div class="b"><img src="/images/img/fruit-lime.gif" alt="image" /> <b><a href="/load">Загрузки</a></b> (<?=stats_load()?>)</div>
 <?=recentfiles()?>
 
 <div class="b"><img src="/images/img/cookie.gif" alt="image" /> <b><a href="/blog">Блоги</a></b> (<?=stats_blog()?>)</div>
 <?=recentblogs()?>
 
 <div class="b"><img src="/images/img/fruit.gif" alt="image" /> <b><a href="/pages/index.php">Сервисы сайта</a></b></div>
-<img src="/images/img/act.png" alt="image" /> <a href="/rotorcms">RotorCMS <?=$config['rotorversion']?></a><br />
-<img src="/images/img/act.png" alt="image" /> <a href="https://github.com/visavi/rotorcms">RCMS 4.0</a> / <a href="https://github.com/visavi/wap-motor">WM 21</a><br />
-
-<img src="/images/img/act.png" alt="image" /> <a href="/services">Мастерская</a><br />
+<img src="/images/img/act.png" alt="image" /> <a href="/pages/userlist.php">Список юзеров</a> (<?=stats_users()?>)<br />
+<img src="/images/img/act.png" alt="image" /> <a href="/pages/adminlist.php">Администрация</a> (<?=stats_admins()?>)<br />
 <img src="/images/img/act.png" alt="image" /> <a href="/pages/index.php?act=stat">Информация</a><br />
-<img src="/images/img/act.png" alt="image" /> <a href="/pages/index.php?act=site">О проекте</a><br />
-<b><a href="/pages/index.php?act=help"><span style="color:#ff0000">Ищем спонсоров!</span></a></b><br />
-
-<?=show_counter()?>
-
+<img src="/images/img/act.png" alt="image" /> <a href="/pages/index.php?act=partners">Партнеры и друзья</a><br />
