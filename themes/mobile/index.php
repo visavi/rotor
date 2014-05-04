b31a13a5941d45c204b307a0480ed0f2cc92bc02
Alexandr Sytnyk <syzenqq@gmail.com>
2014-05-05 00:15:28 +0300

    Добавлена тема mobile

---
+++ themes/mobile/index.php
@@ -0,0 +1,50 @@
+<?php
+#-----------------------------------------------------#
+#          ********* ROTORCMS *********               #
+#              Made by  :  VANTUZ                     #
+#               E-mail  :  visavi.net@mail.ru         #
+#                 Site  :  http://pizdec.ru           #
+#             WAP-Site  :  http://visavi.net          #
+#                  ICQ  :  36-44-66                   #
+#  Вы не имеете право вносить изменения в код скрипта #
+#        для его дальнейшего распространения          #
+#-----------------------------------------------------#
+header('Content-type:text/html; charset=utf-8');
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ru" lang="ru">
+<head>
+    <title>%TITLE%</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="image_src" href="/images/img/icon.png"/>
+    <link rel="stylesheet" href="/themes/mobile/css/style.css" type="text/css"/>
+    <link rel="alternate" href="/news/rss.php" title="RSS News" type="application/rss+xml"/>
+    <?= include_javascript() ?>
+    <meta name="keywords" content="%KEYWORDS%"/>
+    <meta name="description" content="%DESCRIPTION%"/>
+    <meta name="generator" content="RotorCMS <?= $config['rotorversion'] ?>"/>
+</head>
+<body>
+<!--Design by Vantuz (http://pizdec.ru)-->
+
+<table width="600" border="0" cellspacing="0" cellpadding="0" align="center">
+    <tr>
+        <td width="10" height="10" style="background-image:url(/themes/mobile/img/border_top_left.gif);"></td>
+        <td width="580" height="10" style="background-image:url(/themes/mobile/img/border_top.gif);"></td>
+        <td width="10" height="10" style="background-image:url(/themes/mobile/img/border_top_right.gif);"></td>
+    </tr>
+    <tr valign="top">
+        <td width="10" style="background-image:url(/themes/mobile/img/border_left.gif);"></td>
+        <td width="580">
+
+            <div class="a" id="up">
+                <a href="/"><img src="<?= $config['logotip'] ?>" alt="<?= $config['title'] ?>"/></a><br/>
+                <?= $config['logos'] ?>
+            </div>
+
+            <?php render('includes/menu'); ?>
+
+            <div class="site">
+
+                <?php render('includes/note', array('php_self' => $php_self)); ?>
