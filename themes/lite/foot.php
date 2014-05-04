3ad552b166af64cff4b54a8b6652a4c71780ee80
Alexandr Sytnyk <syzenqq@gmail.com>
2014-05-05 00:22:20 +0300

    Добавлена тема lite

---
+++ themes/lite/foot.php
@@ -0,0 +1,23 @@
+<?php
+#-----------------------------------------------------#
+#          ********* RotorCMS *********               #
+#              Made by  :  VANTUZ                     #
+#               E-mail  :  visavi.net@mail.ru         #
+#                 Site  :  http://pizdec.ru           #
+#             WAP-Site  :  http://visavi.net          #
+#                  ICQ  :  36-44-66                   #
+#  Вы не имеете право вносить изменения в код скрипта #
+#        для его дальнейшего распространения          #
+#-----------------------------------------------------#
+?>
+</div>
+<div id="down">
+    <a href="<?= $config['home'] ?>"><?= $config['copy'] ?></a><br/>
+    <?= show_counter() ?>
+    <?= show_online() ?>
+</div>
+<div style="text-align:center">
+    <?= navigation() ?>
+    <?= perfomance() ?>
+</div></div></body></html>
+
