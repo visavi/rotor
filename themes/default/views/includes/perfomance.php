6630178171fb06e2111a7cd3117269b0290b2dbb
visavi <visavi.net@mail.ru>
2014-06-07 17:52:36 +0400

    Поправлен вывод производительности

--- themes/default/views/includes/perfomance.php
+++ themes/default/views/includes/perfomance.php
@@ -1,19 +1,19 @@
 <div class="hide" style="text-align: left;"><b>Статистика производительности</b><br />
 
-MySQL запросов: <?=DB::run() -> queryCounter()?><br />
+MySQL запросов: <?= DB::run() -> queryCounter() ?><br />
 
-Потребление ОЗУ: <?=formatsize(memory_get_usage())?><br />
+Потребление ОЗУ: <?= formatsize(memory_get_usage()) ?><br />
 
 <?php if (function_exists('sys_getloadavg')): ?>
 	<?php $cpu = sys_getloadavg(); ?>
-	Загрузка CPU: <?=$cpu[0]?><br />
+	Загрузка CPU: <?= round($cpu[0], 2) ?><br />
 <?php endif; ?>
 
 <?php if (!empty($config['gzip'])): ?>
 	<?php $compression = Compressor::result(); ?>
-	<?php if (!empty($compression)) {?> Компрессия: <?=$compression?>%<br /> <?php }?>
+	<?php if (!empty($compression)) {?> Компрессия: <?= $compression ?>%<br /> <?php }?>
 <?php endif; ?>
 
-Загрузка страницы: <?=round(microtime(1) - STARTTIME, 4)?> сек.<br />
+Загрузка страницы: <?= round(microtime(1) - STARTTIME, 4) ?> сек.<br />
 </div>
 
