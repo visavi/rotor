4020b0b819247f469b1a5e23e536e888372ffeb7
visavi <visavi.net@mail.ru>
2016-06-10 14:04:36 +0300

    Изменил стили пагинации

--- assets/views/includes/pagination.php
+++ assets/views/includes/pagination.php
@@ -1,12 +1,13 @@
-<div class="nav">
-	Страницы:
-	<?php foreach($pages as $page): ?>
-		<?php if(isset($page['separator'])): ?>
-			<?= $page['name'] ?>
-		<?php elseif(isset($page['current'])): ?>
-			<span class="navcurrent"><?= $page['name'] ?></span>
-		<?php else: ?>
-			<a href="<?= $url ?>start=<?= $page['start'] ?>" title="<?= $page['title'] ?>"><?= $page['name'] ?></a>
-		<?php endif; ?>
-	<?php endforeach; ?>
-</div>
+<nav>
+	<ul class="pagination pagination-sm">
+		<?php foreach($pages as $page): ?>
+			<?php if(isset($page['separator'])): ?>
+				<li><span><?= $page['name'] ?></span></li>
+			<?php elseif(isset($page['current'])): ?>
+				<li class="active"><span data-toggle="tooltip" title="Текущая"><?= $page['name'] ?></span></li>
+			<?php else: ?>
+				<li><a href="<?= $url ?>start=<?= $page['start'] ?>" data-toggle="tooltip" title="<?= $page['title'] ?>"><?= $page['name'] ?></a></li>
+			<?php endif; ?>
+		<?php endforeach; ?>
+	</ul>
+</nav>
