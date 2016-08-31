ab14f3d8beefafd71f95ba034bad1ef9025dbb98
visavi <visavi.net@mail.ru>
2016-08-31 18:37:20 +0300

    Поправил главную

--- includes/routes.php
+++ includes/routes.php
@@ -4,7 +4,10 @@ $router = new AltoRouter();
 
 $router->addMatchTypes(['user' => '[0-9A-Za-z-_]++', 'slug' => '[0-9a-z-_]++']);
 
-$router->map('GET', '/', '/modules/index.php', 'home');
+$router->map( 'GET', '/', function() {
+    App::view('index');
+});
+
 $router->map('GET', '/book', '/modules/book/index.php', 'book');
 $router->map('POST', '/book/[add|edit:action]', '/modules/book/index.php');
 
