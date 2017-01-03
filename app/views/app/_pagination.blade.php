7ee001ca949cbf4a63814f22bdfccfd828512296
visavi <visavi.net@mail.ru>
2017-01-03 04:05:24 +0300

    Поправлена постраничная навигация

--- app/views/app/_pagination.blade.php
+++ app/views/app/_pagination.blade.php
@@ -1,12 +1,12 @@
 <nav>
-	<ul class="pagination">
+	<ul class="pagination pagination-sm">
 		@foreach($pages as $page)
 			@if(isset($page['separator']))
 				<li><span>{{ $page['name'] }}</span></li>
 			@elseif(isset($page['current']))
-				<li class="active"><span data-toggle="tooltip" title="Текущая">{{ $page['name'] }}</span></li>
+				<li class="active"><span>{{ $page['name'] }}</span></li>
 			@else
-				<li><a href="?page={{ $page['page'] }}{{ $request }}" data-toggle="tooltip" title="{{ $page['title'] }}">{{ $page['name'] }}</a></li>
+				<li><a href="?page={{ $page['page'] }}{{ $request }}">{{ $page['name'] }}</a></li>
 			@endif
 		@endforeach
 	</ul>
