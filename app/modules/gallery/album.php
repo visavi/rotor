2fec5f2d78e82bc285992424dc0268e7d5a977e0
visavi <visavi.net@mail.ru>
2017-08-02 00:38:37 +0300

    Личный альбом пользователя

--- app/modules/gallery/album.php
+++ app/modules/gallery/album.php
@@ -42,47 +42,15 @@ switch ($act):
 
         $page = App::paginate(App::setting('fotolist'), $total);
 
-        if ($total > 0) {
+        $photos = Photo::where('user_id', $user->id)
+            ->offset($page['offset'])
+            ->limit($page['limit'])
+            ->orderBy('created_at', 'desc')
+            ->get();
 
-            //App::setting('newtitle') = 'Список всех фотографий '.$uz.' (Стр. '.$page['current'].')';
+        $moder = (App::getUserId() == $user->id) ? 1 : 0;
 
-            $photos = Photo::where('user_id', $user->id)
-                ->offset($page['offset'])
-                ->limit($page['limit'])
-                ->orderBy('created_at', 'desc')
-                ->get();
-
-            $moder = (App::getUserId() == $user->id) ? 1 : 0;
-
-            foreach ($photos as $data) {
-                echo '<div class="b"><i class="fa fa-picture-o"></i> ';
-                echo '<b><a href="/gallery?act=view&amp;gid='.$data['id'].'&amp;page='.$page['current'].'">'.$data['title'].'</a></b> ('.read_file(HOME.'/uploads/pictures/'.$data['link']).')<br />';
-
-                if (!empty($moder)) {
-                    echo '<a href="/gallery?act=edit&amp;gid='.$data['id'].'&amp;page='.$page['current'].'">Редактировать</a> / ';
-                    echo '<a href="/gallery?act=delphoto&amp;gid='.$data['id'].'&amp;page='.$page['current'].'&amp;uid='.$_SESSION['token'].'">Удалить</a>';
-                }
-
-                echo '</div><div>';
-                echo '<a href="/gallery?act=view&amp;gid='.$data['id'].'&amp;page='.$page['current'].'">'.resize_image('uploads/pictures/', $data['link'], App::setting('previewsize'), ['alt' => $data['title']]).'</a><br />';
-
-                if (!empty($data['text'])){
-                    echo App::bbCode($data['text']).'<br />';
-                }
-
-                echo 'Добавлено: '.profile($data['user']).' ('.date_fixed($data['created_at']).')<br />';
-                echo '<a href="/gallery?act=comments&amp;gid='.$data['id'].'">Комментарии</a> ('.$data['comments'].')';
-                echo '</div>';
-            }
-
-            App::pagination($page);
-
-            echo 'Всего фотографий: <b>'.$total.'</b><br /><br />';
-        } else {
-            show_error('Фотографий в альбоме еще нет!');
-        }
-
-        echo '<i class="fa fa-arrow-circle-up"></i> <a href="/gallery/album">Альбомы</a><br />';
+        App::view('gallery/album_photo', compact('photos', 'moder', 'page', 'user'));
     break;
 
 endswitch;
