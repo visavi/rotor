979932b23f7e90eabe12eaa13d89faa23b419c9b
visavi <visavi.net@mail.ru>
2017-08-08 12:40:23 +0300

    Тест гостевой книги

---
+++ tests/GuestTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class GuestTest extends TestCase
+{
+    public function testGuest()
+    {
+        $guest = new Guest();
+        $guest->user_id = 1;
+        $guest->text = 'Test text message';
+        $guest->ip = '127.0.0.1';
+        $guest->brow = 'Chrome 60.0';
+        $guest->created_at = SITETIME;
+        $guest->save();
+
+        $this->assertTrue($guest->save());
+
+        $getGuest = Guest::find($guest->id);
+        $this->assertEquals($getGuest->text, 'Test text message');
+
+        $guest->update(['text' => 'Test simple message']);
+
+        $getGuest = Guest::find($guest->id);
+        $this->assertEquals($getGuest->text, 'Test simple message');
+
+        $guest->delete();
+
+        $getGuest = Guest::find($guest->id);
+        $this->assertNull($getGuest);
+    }
+}
