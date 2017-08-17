af1f0a7e07f7ecddf1d0009ed341235053deed55
visavi <visavi.net@mail.ru>
2017-08-17 14:47:41 +0300

    поправил стили постраничной навигации

--- app/views/app/_pagination.blade.php
+++ app/views/app/_pagination.blade.php
@@ -1,13 +1,13 @@
 <nav>
-	<ul class="pagination pagination-sm">
-		@foreach($pages as $page)
-			@if(isset($page['separator']))
-				<li><span>{{ $page['name'] }}</span></li>
-			@elseif(isset($page['current']))
-				<li class="active"><span>{{ $page['name'] }}</span></li>
-			@else
-				<li><a href="?page={{ $page['page'] }}{{ $request }}">{{ $page['name'] }}</a></li>
-			@endif
-		@endforeach
-	</ul>
+    <ul class="pagination pagination-sm">
+        @foreach($pages as $page)
+            @if(isset($page['separator']))
+                <li class="page-item disabled"><span class="page-link">{{ $page['name'] }}</span></li>
+            @elseif(isset($page['current']))
+                <li class="page-item active"><span class="page-link">{{ $page['name'] }}</span></li>
+            @else
+                <li class="page-item"><a class="page-link" href="?page={{ $page['page'] }}{{ $request }}">{{ $page['name'] }}</a></li>
+            @endif
+        @endforeach
+    </ul>
 </nav>
