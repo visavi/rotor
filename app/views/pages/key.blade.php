97b921bf75c6f0c5606a186d2c8d357d541d8e25
Alexandr Sytnyk <sy2nyk@gmail.com>
2017-08-29 12:12:10 +0300

    шаблоны на страницах сайта

--- app/views/pages/key.blade.php
+++ app/views/pages/key.blade.php
@@ -5,30 +5,32 @@
 @stop
 
 @section('content')
-    <?php
 
-    if (App::user('confirmreg') == 1) {
 
-        echo '<h1>Подтверждение регистрации</h1>';
-        echo 'Добро пожаловать, <b>'.App::getUsername().'!</b><br>';
-        echo 'Для подтверждения регистрации вам необходимо ввести мастер-ключ, который был отправлен вам на email<br><br>';
+    @if (App::user('confirmreg') == 1)
 
-        echo '<div class="form">';
-        echo 'Мастер-код:<br>';
-        echo '<form method="get" action="/key">';
-        echo '<input  class="form-control" name="code" maxlength="30">';
-        echo '<button class="btn btn-primary">Подтвердить</button></form></div><br>';
+        <h1>Подтверждение регистрации</h1>
+        Добро пожаловать, <b>{{ App::getUsername() }}!</b><br>
+        Для подтверждения регистрации вам необходимо ввести мастер-ключ, который был отправлен вам на email<br><br>
 
-        echo 'Пока вы не подтвердите регистрацию вы не сможете войти на сайт<br>';
-        echo 'Ваш профиль будет ждать активации в течение 24 часов, после чего автоматически удален<br><br>';
+        <div class="form">
+            Мастер-код:<br>
+            <form method="get" action="/key">
+                <input class="form-control" name="code" maxlength="30">
+                <button class="btn btn-primary">Подтвердить</button>
+            </form>
+        </div><br>
 
-    } else {
-        echo '<h1>Ожидание модерации</h1>';
-        echo 'Добро пожаловать, <b>'.check(App::getUsername()).'!</b><br>';
-        echo 'Ваш аккаунт еще не прошел проверку администрацией<br>';
-        echo 'Если после авторизации вы видите эту страницу, значит ваш профиль еще не активирован!<br><br>';
-    }
+        Пока вы не подтвердите регистрацию вы не сможете войти на сайт<br>
+        Ваш профиль будет ждать активации в течение 24 часов, после чего автоматически удален<br><br>
 
-    echo '<i class="fa fa-times"></i> <a href="/logout">Выход</a><br>';
-    ?>
+    @else
+        <h1>Ожидание модерации</h1>
+        Добро пожаловать, <b>{{ App::getUsername() }}!</b><br>
+        Ваш аккаунт еще не прошел проверку администрацией<br>
+        Если после авторизации вы видите эту страницу, значит ваш профиль еще не активирован!<br><br>
+    @endif
+
+    <i class="fa fa-times"></i> <a href="/logout">Выход</a><br>
+
 @stop
