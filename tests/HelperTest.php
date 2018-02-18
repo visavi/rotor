70577b7ee5db55edea52f21f7faabbdd60125557
visavi <visavi.net@mail.ru>
2018-02-18 21:49:23 +0300

    тесты

---
+++ tests/HelperTest.php
@@ -0,0 +1,45 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Models\Guest;
+
+class HelperTest extends TestCase
+{
+    public function testMakeTime()
+    {
+        $makeTime = makeTime(100);
+        $this->assertSame($makeTime, '01:40');
+
+        $makeTime = makeTime(5000);
+        $this->assertSame($makeTime, '01:23:20');
+    }
+
+    public function testDateFixed()
+    {
+        $timestamp = 1117627200;
+
+        $this->assertSame(dateFixed($timestamp), '01.06.05 / 12:00');
+
+        $this->assertSame(dateFixed($timestamp, 'Y-m-d'), '2005-06-01');
+
+        $this->assertSame(dateFixed(false), dateFixed(SITETIME));
+    }
+
+    public function testFormatTime()
+    {
+        $formatTime = formatTime(0);
+        $this->assertSame($formatTime, 0);
+
+        $formatTime = formatTime(60);
+        $this->assertSame($formatTime, '1 минута');
+
+        $formatTime = formatTime(3600);
+        $this->assertSame($formatTime, '1 час');
+
+        $formatTime = formatTime(86400);
+        $this->assertSame($formatTime, '1 день');
+
+        $formatTime = formatTime(86400 * 365);
+        $this->assertSame($formatTime, '1 год');
+    }
+}
