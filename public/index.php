77b92009d67ac8e61221a64b7a9557a2e134c9a4
visavi <visavi.net@mail.ru>
2018-03-04 23:11:57 +0300

    убрал старый код

--- public/index.php
+++ public/index.php
@@ -7,11 +7,7 @@
  * @link      http://visavi.net
  * @copyright 2005-2018
  */
-use Illuminate\Database\Capsule\Manager as DB;
+
 include_once __DIR__.'/../app/start.php';
-
-
-/*DB::update('update forums set last_topic_id = (select id from topics where forums.id = topics.forum_id order by updated_at desc limit 1)');*/
-
 
 App\Classes\Application::run();
