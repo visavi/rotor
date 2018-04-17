c69df64234d483b8e3286e999fdd88b957cdaef3
visavi <visavi.net@mail.ru>
2018-04-18 01:44:23 +0300

    рефакторинг

--- app/helpers.php
+++ app/helpers.php
@@ -2543,27 +2543,29 @@ function uniqueName(string $extension = null): string
  */
 function getCourses()
 {
-    if (@filemtime(STORAGE."/temp/courses.dat") < time() - 3600 || @filesize(STORAGE."/temp/courses.dat") == 0) {
+    if (
+        @filemtime(STORAGE . '/temp/courses.dat') < time() - 3600 ||
+        @filesize(STORAGE . '/temp/courses.dat') == 0
+    ) {
         $curl = new Curl();
-
         if ($xml = $curl->get('http://www.cbr.ru/scripts/XML_daily.asp')){
 
             $courses = [];
             $courses['Date'] = (string) $xml->attributes()->Date;
-            foreach ($xml->Valute as $item) {
 
+            foreach ($xml->Valute as $item) {
                 $courses[(string) $item->CharCode] = [
-                    'name' => (string) $item->Name,
-                    'value' => (string) $item->Value,
+                    'name'    => (string) $item->Name,
+                    'value'   => (string) $item->Value,
                     'nominal' => (string) $item->Nominal,
                 ];
             }
 
-            file_put_contents(STORAGE."/temp/courses.dat", json_encode($courses), LOCK_EX);
+            file_put_contents(STORAGE . '/temp/courses.dat', json_encode($courses), LOCK_EX);
         }
     }
 
-    $courses = @json_decode(file_get_contents(STORAGE."/temp/courses.dat"));
+    $courses = @json_decode(file_get_contents(STORAGE . '/temp/courses.dat'));
 
     return view('app/_courses', compact('courses'));
 }
