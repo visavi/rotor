82eaf7206dbc280f0fdacbf2e9ac1a36bfc3c851
visavi <visavi.net@mail.ru>
2018-04-18 19:28:47 +0300

    рефакторинг

--- app/start.php
+++ app/start.php
@@ -6,7 +6,7 @@ use App\Models\Login;
 use App\Models\User;
 use Illuminate\Database\Capsule\Manager as DB;
 
-require __DIR__.'/bootstrap.php';
+require __DIR__ . '/bootstrap.php';
 
 ob_start();
 session_name('SID');
@@ -18,19 +18,19 @@ date_default_timezone_set(setting('timezone'));
  * Авторизация по кукам
  */
 if (empty($_SESSION['id']) && empty($_SESSION['password'])) {
-    if (isset($_COOKIE['login']) && isset($_COOKIE['password'])) {
+    if (isset($_COOKIE['login'], $_COOKIE['password'])) {
 
         $cookLogin = check($_COOKIE['login']);
-        $cookPass = check($_COOKIE['password']);
+        $cookPass  = check($_COOKIE['password']);
 
         $user = User::query()->where('login', $cookLogin)->first();
 
         if ($user) {
-            if ($cookLogin == $user->login && $cookPass == md5($user->password.env('APP_KEY'))) {
+            if ($cookLogin === $user->login && $cookPass === md5($user->password . env('APP_KEY'))) {
                 session_regenerate_id(1);
 
                 $_SESSION['id']       = $user->id;
-                $_SESSION['password'] = md5(env('APP_KEY').$user->password);
+                $_SESSION['password'] = md5(env('APP_KEY') . $user->password);
 
                 $authorization = Login::query()
                     ->where('user_id', $user->id)
@@ -79,14 +79,14 @@ if ($user = checkAuth()) {
     // Забанен
     if ($user->level == User::BANNED) {
         if (! Request::is('ban', 'rules', 'logout')) {
-            redirect('/ban?user='.$user->login);
+            redirect('/ban?user=' . $user->login);
         }
     }
 
     // Подтверждение регистрации
     if (setting('regkeys') && $user->level == User::PENDED) {
         if (! Request::is('key', 'ban', 'login', 'logout')) {
-            redirect('/key?user='.$user->login);
+            redirect('/key?user=' . $user->login);
         }
     }
 
@@ -94,10 +94,10 @@ if ($user = checkAuth()) {
     if ($user->timebonus < SITETIME - 82800) {
         $user->update([
             'timebonus' => SITETIME,
-            'money'     => DB::raw('money + '.setting('bonusmoney')),
+            'money'     => DB::raw('money + ' . setting('bonusmoney')),
         ]);
 
-        setFlash('success', 'Получен ежедневный бонус '.plural(setting('bonusmoney'), setting('moneyname')).'!');
+        setFlash('success', 'Получен ежедневный бонус ' . plural(setting('bonusmoney'), setting('moneyname')) . '!');
     }
 }
 
@@ -114,15 +114,15 @@ if (! getUser() || empty($setting['themes'])) {
     }
 }
 
-if (empty($setting['themes']) || ! file_exists(HOME.'/themes/'.$setting['themes'])) {
+if (empty($setting['themes']) || ! file_exists(HOME . '/themes/' . $setting['themes'])) {
     $setting['themes'] = 'default';
 }
 
-if (empty($setting['lang']) || ! file_exists(RESOURCES.'/lang/'.$setting['lang'])) {
+if (empty($setting['lang']) || ! file_exists(RESOURCES . '/lang/' . $setting['lang'])) {
     $setting['lang'] = 'ru';
 }
 
-$files = glob(APP.'/plugins/*.php');
+$files = glob(APP . '/Plugins/*.php');
 foreach ($files as $file) {
     require_once $file;
 }
