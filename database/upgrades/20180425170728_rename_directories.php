7725605b75c32cbbe6450025fa4a63d004525573
visavi <visavi.net@mail.ru>
2018-04-25 17:29:41 +0300

    миграция для изменения директорий

--- database/upgrades/20180425170728_rename_directories.php
+++ database/upgrades/20180425170728_rename_directories.php
@@ -9,7 +9,15 @@ class RenameDirectories extends AbstractMigration
      */
     public function up()
     {
+        @rename(STORAGE . '/backup', STORAGE . '/backups');
+        @rename(STORAGE . '/cache', STORAGE . '/caches');
 
+        @rename(UPLOADS . '/forum', UPLOADS . '/forums');
+        @rename(UPLOADS . '/screen', UPLOADS . '/screens');
+        @rename(UPLOADS . '/thumbnail', UPLOADS . '/thumbnails');
+        @rename(UPLOADS . '/photos', UPLOADS . '/photos_temp');
+        @rename(UPLOADS . '/pictures', UPLOADS . '/photos');
+        @rename(UPLOADS . '/photos_temp', UPLOADS . '/pictures');
     }
 
     /**
@@ -17,6 +25,14 @@ class RenameDirectories extends AbstractMigration
      */
     public function down()
     {
+        @rename(STORAGE . '/backups', STORAGE . '/backup');
+        @rename(STORAGE . '/caches', STORAGE . '/cache');
 
+        @rename(UPLOADS . '/forums', UPLOADS . '/forum');
+        @rename(UPLOADS . '/screens', UPLOADS . '/screen');
+        @rename(UPLOADS . '/thumbnails', UPLOADS . '/thumbnail');
+        @rename(UPLOADS . '/pictures', UPLOADS . '/pictures_temp');
+        @rename(UPLOADS . '/photos', UPLOADS . '/pictures');
+        @rename(UPLOADS . '/pictures_temp', UPLOADS . '/photos');
     }
 }
