0af21947a830656b8aff6e69fe8cdae11601931b
visavi <visavi.net@mail.ru>
2018-04-26 03:02:39 +0300

    fix models

--- app/Models/Counter24.php
+++ app/Models/Counter24.php
@@ -5,6 +5,13 @@ namespace App\Models;
 class Counter24 extends BaseModel
 {
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'counter24';
+
+    /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
--- app/Models/Counter31.php
+++ app/Models/Counter31.php
@@ -5,6 +5,13 @@ namespace App\Models;
 class Counter31 extends BaseModel
 {
     /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'counter31';
+
+    /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
