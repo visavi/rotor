20977a6d6cf418e6715b54a7ae1bb60610bb5e3d
visavi <visavi.net@mail.ru>
2018-06-08 18:29:53 +0300

    оптимизировал sitemap

--- app/Controllers/SitemapController.php
+++ app/Controllers/SitemapController.php
@@ -42,20 +42,20 @@ class SitemapController extends BaseController
         $blogs = Blog::query()
             ->selectRaw('blogs.*, max(c.created_at) as last_time')
             ->leftJoin('comments as c', function (JoinClause $join) {
-                $join->on('blogs.id', '=', 'c.relate_id')
-                    ->where('relate_type', '=', Blog::class);
+                $join->on('blogs.id', 'c.relate_id')
+                    ->where('relate_type', Blog::class);
             })
             ->groupBy('blogs.id')
             ->orderBy('last_time', 'desc')
             ->get();
 
         $locs = [];
-        foreach ($blogs as $blog) {
 
+        foreach ($blogs as $blog) {
             $changeTime = ($blog->last_time > $blog->created_at) ? $blog->last_time : $blog->created_at;
 
             // Обновлено менее 1 месяца
-            $new = (SITETIME < $changeTime + 3600 * 24 * 30) ? true : false;
+            $new = SITETIME < strtotime('+1 month', $changeTime);
 
             $locs[] = [
                 'loc'        => siteUrl(true) . '/articles/' . $blog->id,
@@ -64,6 +64,7 @@ class SitemapController extends BaseController
                 'priority'   => $new ? '1.0' : '0.5',
             ];
         }
+
         return view('sitemap/url', compact('locs'));
     }
     /**
@@ -74,20 +75,20 @@ class SitemapController extends BaseController
         $newses = News::query()
             ->selectRaw('news.*, max(c.created_at) as last_time')
             ->leftJoin('comments as c', function (JoinClause $join) {
-                $join->on('news.id', '=', 'c.relate_id')
-                    ->where('relate_type', '=', News::class);
+                $join->on('news.id', 'c.relate_id')
+                    ->where('relate_type', News::class);
             })
             ->groupBy('news.id')
             ->orderBy('last_time', 'desc')
             ->get();
 
         $locs = [];
-        foreach ($newses as $news) {
 
+        foreach ($newses as $news) {
             $changeTime = ($news->last_time > $news->created_at) ? $news->last_time : $news->created_at;
 
             // Обновлено менее 1 месяца
-            $new = (SITETIME < $changeTime + 3600 * 24 * 30) ? true : false;
+            $new = SITETIME < strtotime('+1 month', $changeTime);
 
             $locs[] = [
                 'loc'        => siteUrl(true) . '/news/' . $news->id,
@@ -96,6 +97,7 @@ class SitemapController extends BaseController
                 'priority'   => $new ? '1.0' : '0.5',
             ];
         }
+
         return view('sitemap/url', compact('locs'));
     }
 
@@ -107,10 +109,10 @@ class SitemapController extends BaseController
         $topics = Topic::query()->orderBy('updated_at', 'desc')->limit(15000)->get();
 
         $locs = [];
-        foreach ($topics as $topic) {
 
+        foreach ($topics as $topic) {
             // Обновлено менее 1 месяца
-            $new = (SITETIME < $topic->updated_at + 3600 * 24 * 30) ? true : false;
+            $new = SITETIME < strtotime('+1 month', $topic->updated_at);
 
             $locs[] = [
                 'loc'        => siteUrl(true) . '/topics/' . $topic->id,
@@ -130,8 +132,8 @@ class SitemapController extends BaseController
         $downs = Down::query()
             ->selectRaw('downs.*, max(c.created_at) as last_time')
             ->leftJoin('comments as c', function (JoinClause $join) {
-                $join->on('downs.id', '=', 'c.relate_id')
-                    ->where('relate_type', '=', Down::class);
+                $join->on('downs.id', 'c.relate_id')
+                    ->where('relate_type', Down::class);
             })
             ->groupBy('downs.id')
             ->orderBy('last_time', 'desc')
@@ -143,7 +145,7 @@ class SitemapController extends BaseController
             $changeTime = ($down->last_time > $down->created_at) ? $down->last_time : $down->created_at;
 
             // Обновлено менее 1 месяца
-            $new = (SITETIME < $changeTime + 3600 * 24 * 30) ? true : false;
+            $new = SITETIME < strtotime('+1 month', $changeTime);
 
             $locs[] = [
                 'loc'        => siteUrl(true) . '/downs/' . $down->id,
@@ -152,6 +154,7 @@ class SitemapController extends BaseController
                 'priority'   => $new ? '1.0' : '0.5',
             ];
         }
+
         return view('sitemap/url', compact('locs'));
     }
 
