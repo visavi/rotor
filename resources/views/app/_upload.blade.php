8bdc6b16850a604103aa66b8b82fd1bf3f8ede70
visavi <visavi.net@mail.ru>
2018-06-30 23:43:01 +0300

    поправил условие

--- resources/views/app/_upload.blade.php
+++ resources/views/app/_upload.blade.php
@@ -1,13 +1,13 @@
 <?php
-$condition = $paste ?? 0;
-$onclick = $condition ? 'return pasteImage(this);' : false;
+$cond  = ! empty($paste);
+$click = $cond ? 'return pasteImage(this);' : false;
 ?>
 
 <div class="js-images">
     @if ($files->isNotEmpty())
         @foreach ($files as $file)
             <span class="js-image">
-                {!! resizeImage($file->hash, ['width' => 100, 'onclick' => $onclick]) !!}
+                {!! resizeImage($file->hash, ['width' => 100, 'onclick' => $click]) !!}
                 <a href="#" onclick="return deleteImage(this);" data-id="{{ $file->id }}" data-type="{{ $type }}" data-token="{{ $_SESSION['token'] }}"><i class="fas fa-times"></i></a>
             </span>
         @endforeach
@@ -16,12 +16,12 @@ $onclick = $condition ? 'return pasteImage(this);' : false;
 
 <div class="js-image-template d-none">
     <span class="js-image">
-        <img src="#" width="100" onclick="{{ $onclick }}" alt="" class="img-fluid">
+        <img src="#" width="100" onclick="{{ $click }}" alt="" class="img-fluid">
         <a href="#" onclick="return deleteImage(this);" data-type="{{ $type }}" data-token="{{ $_SESSION['token'] }}"><i class="fas fa-times"></i></a>
     </span>
 </div>
 
 <label class="btn btn-sm btn-secondary" for="image">
-    <input id="image" type="file" name="image" onchange="return submitImage(this, {{ $condition }});" data-id="{{ $id or 0 }}" data-type="{{ $type }}" data-token="{{ $_SESSION['token'] }}" hidden>
+    <input id="image" type="file" name="image" onchange="return submitImage(this, {{ $cond }});" data-id="{{ $id or 0 }}" data-type="{{ $type }}" data-token="{{ $_SESSION['token'] }}" hidden>
     Прикрепить картинку&hellip;
 </label><br>
