4b79fd2abdc0540430a32eba8421bf5ed9dbf7ef
visavi <visavi.net@mail.ru>
2018-07-18 18:47:37 +0300

    удалил забытые тег

--- resources/views/admin/photos/index.blade.php
+++ resources/views/admin/photos/index.blade.php
@@ -24,60 +24,59 @@
     </nav>
 
     @if ($photos->isNotEmpty())
-            <input type="hidden" name="token" value="{{ $_SESSION['token'] }}">
+        <input type="hidden" name="token" value="{{ $_SESSION['token'] }}">
 
-            @foreach ($photos as $photo)
-                <div class="b">
-                    <i class="fa fa-image"></i>
-                    <b><a href="/photos/{{ $photo->id }}">{{ $photo->title }}</a></b>
+        @foreach ($photos as $photo)
+            <div class="b">
+                <i class="fa fa-image"></i>
+                <b><a href="/photos/{{ $photo->id }}">{{ $photo->title }}</a></b>
 
-                    <div class="float-right">
-                        <a href="/admin/photos/edit/{{ $photo->id }}?page={{ $page->current }}"><i class="fas fa-pencil-alt text-muted"></i></a>
-                        <a href="/admin/photos/delete/{{ $photo->id }}?page={{ $page->current }}&amp;token={{ $_SESSION['token'] }}"><i class="fas fa-times text-muted"></i></a>
-                    </div>
+                <div class="float-right">
+                    <a href="/admin/photos/edit/{{ $photo->id }}?page={{ $page->current }}"><i class="fas fa-pencil-alt text-muted"></i></a>
+                    <a href="/admin/photos/delete/{{ $photo->id }}?page={{ $page->current }}&amp;token={{ $_SESSION['token'] }}"><i class="fas fa-times text-muted"></i></a>
                 </div>
+            </div>
 
-                <div>
-                    <?php $countFiles = $photo->files->count() ?>
-                    <div id="myCarousel{{ $loop->iteration }}" class="carousel slide" data-ride="carousel">
-                        @if ($countFiles > 1)
-                            <ol class="carousel-indicators">
-                                @for ($i = 0; $i < $countFiles; $i++)
-                                    <li data-target="#myCarousel{{ $loop->iteration }}" data-slide-to="{{ $i }}"{!! empty($i) ? ' class="active"' : '' !!}></li>
-                                @endfor
-                            </ol>
-                        @endif
-
-                        <div class="carousel-inner">
-                            @foreach ($photo->files as $file)
-                                <div class="carousel-item{{ $loop->first ? ' active' : '' }}">
-                                    <a href="/photos/{{ $photo->id }}">{!! resizeImage($file->hash, ['alt' => $photo->title, 'class' => 'd-block w-100']) !!}</a>
-                                </div>
-                            @endforeach
-                        </div>
-
-                        @if ($countFiles > 1)
-                            <a class="carousel-control-prev" href="#myCarousel{{ $loop->iteration }}" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#myCarousel{{ $loop->iteration }}" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                        @endif
-                    </div>
-
-                    @if ($photo->text)
-                        {!! bbCode($photo->text) !!}<br>
+            <div>
+                <?php $countFiles = $photo->files->count() ?>
+                <div id="myCarousel{{ $loop->iteration }}" class="carousel slide" data-ride="carousel">
+                    @if ($countFiles > 1)
+                        <ol class="carousel-indicators">
+                            @for ($i = 0; $i < $countFiles; $i++)
+                                <li data-target="#myCarousel{{ $loop->iteration }}" data-slide-to="{{ $i }}"{!! empty($i) ? ' class="active"' : '' !!}></li>
+                            @endfor
+                        </ol>
                     @endif
 
-                    Добавлено: {!! $photo->user->getProfile() !!} ({{ dateFixed($photo->created_at) }})<br>
-                    <a href="/photos/comments/{{ $photo->id }}">Комментарии</a> ({{ $photo->count_comments }})
-                    <a href="/photos/end/{{ $photo->id }}">&raquo;</a>
+                    <div class="carousel-inner">
+                        @foreach ($photo->files as $file)
+                            <div class="carousel-item{{ $loop->first ? ' active' : '' }}">
+                                <a href="/photos/{{ $photo->id }}">{!! resizeImage($file->hash, ['alt' => $photo->title, 'class' => 'd-block w-100']) !!}</a>
+                            </div>
+                        @endforeach
+                    </div>
+
+                    @if ($countFiles > 1)
+                        <a class="carousel-control-prev" href="#myCarousel{{ $loop->iteration }}" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#myCarousel{{ $loop->iteration }}" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    @endif
                 </div>
-            @endforeach
-        </form>
+
+                @if ($photo->text)
+                    {!! bbCode($photo->text) !!}<br>
+                @endif
+
+                Добавлено: {!! $photo->user->getProfile() !!} ({{ dateFixed($photo->created_at) }})<br>
+                <a href="/photos/comments/{{ $photo->id }}">Комментарии</a> ({{ $photo->count_comments }})
+                <a href="/photos/end/{{ $photo->id }}">&raquo;</a>
+            </div>
+        @endforeach
 
         {!! pagination($page) !!}
 
