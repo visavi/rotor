08b91af4ddc1bbd030689df606b7db88cdd8f85a
visavi <visavi.net@mail.ru>
2019-03-05 00:04:21 +0300

    мелкие правки

--- resources/lang/en/socials.php
+++ resources/lang/en/socials.php
@@ -4,7 +4,7 @@ return [
     'title'         => 'Social networks',
     'added'         => 'Added',
     'total'         => 'Total',
-    'text_choise'   => 'Select a social network to bind',
+    'text_choice'   => 'Select a social network to bind',
     'text_confirm'  => 'Are you sure you want to remove the link',
     'empty_records' => 'No links to social networks',
 ];
