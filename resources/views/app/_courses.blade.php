074036e4803d46cf507cfd920d35c0b59dea2657
visavi <visavi.net@mail.ru>
2019-03-05 18:13:13 +0300

    Курсы валют без наименования, показывается разница курса

--- resources/views/app/_courses.blade.php
+++ resources/views/app/_courses.blade.php
@@ -1,9 +1,23 @@
-@if ($courses)
+<?php
 
+function trend($currency)
+{
+    if ($currency->Value > $currency->Previous) {
+        return ' <i class="fas fa-caret-up text-success"></i>';
+    }
+    if ($currency->Value < $currency->Previous) {
+        return ' <i class="fas fa-caret-down text-danger"></i>';
+    }
+
+    return '';
+}
+?>
+
+@if ($courses)
     <b>{{ trans('index.courses') }}</b> ({{ date('d.m.Y', strtotime($courses->Date)) }})<br>
-    <b>{{ $courses->Valute->USD->Nominal }} {{ $courses->Valute->USD->Name }}</b> - {{ $courses->Valute->USD->Value }}<br>
-    <b>{{ $courses->Valute->EUR->Nominal }} {{ $courses->Valute->EUR->Name }}</b> - {{ $courses->Valute->EUR->Value }}<br>
-    <b>{{ $courses->Valute->UAH->Nominal }} {{ $courses->Valute->UAH->Name }}</b> - {{ $courses->Valute->UAH->Value }}<br>
+    <b>{{ $courses->Valute->USD->Nominal }} {{ $courses->Valute->USD->CharCode }}</b> - {{ $courses->Valute->USD->Value }}{!! trend($courses->Valute->USD) !!}<br>
+    <b>{{ $courses->Valute->EUR->Nominal }} {{ $courses->Valute->EUR->CharCode }}</b> - {{ $courses->Valute->EUR->Value }}{!! trend($courses->Valute->EUR) !!}<br>
+    <b>{{ $courses->Valute->UAH->Nominal }} {{ $courses->Valute->UAH->CharCode }}</b> - {{ $courses->Valute->UAH->Value }}{!! trend($courses->Valute->UAH) !!}<br>
 
 @else
     {!! showError(trans('index.courses_error')) !!}
