bab84c97c0098111ab46ab9ab5a135bd3097d627
visavi <visavi.net@mail.ru>
2019-03-19 19:01:34 +0300

    рефакторинг тесов

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__ . '/../app/bootstrap.php';
-
-$classLoader = new \Composer\Autoload\ClassLoader();
-$classLoader->addPsr4('Tests\\', __DIR__, true);
-$classLoader->register();
-
-
