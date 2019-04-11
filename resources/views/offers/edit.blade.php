d574fb630550f4c87c5483964d18153d1b45b3ac
visavi <visavi.net@mail.ru>
2019-04-12 01:32:19 +0300

    локализация предложений

--- resources/views/offers/edit.blade.php
+++ resources/views/offers/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('title')
-    Редактирование записи
+    {{ trans('offers.editing_record') }}
 @stop
 
 @section('breadcrumb')
@@ -9,7 +9,7 @@
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="/"><i class="fas fa-home"></i></a></li>
             <li class="breadcrumb-item"><a href="/offers/{{ $offer->type }}">{{ trans('offers.title') }}</a></li>
-            <li class="breadcrumb-item active">Редактирование</li>
+            <li class="breadcrumb-item active">{{ trans('offers.editing_record') }}</li>
         </ol>
     </nav>
 @stop
@@ -20,30 +20,30 @@
             <input type="hidden" name="token" value="{{ $_SESSION['token'] }}">
 
             <div class="form-group{{ hasError('type') }}">
-                <label for="types">Тип:</label>
+                <label for="types">{{ trans('offers.i_want_to') }}</label>
 
                 <?php $inputType = getInput('type', $offer->type); ?>
                 <select class="form-control" name="type" id="type">
-                    <option value="offer"{{ $inputType === 'offer' ? ' selected' : '' }}>Предложение</option>
-                    <option value="issue"{{ $inputType === 'issue' ? ' selected' : '' }}>Проблема</option>
+                    <option value="offer"{{ $inputType === 'offer' ? ' selected' : '' }}>{{ trans('offers.suggest_idea') }}</option>
+                    <option value="issue"{{ $inputType === 'issue' ? ' selected' : '' }}>{{ trans('offers.report_problem') }}</option>
                 </select>
 
                 {!! textError('type') !!}
             </div>
 
             <div class="form-group{{ hasError('title') }}">
-                <label for="inputTitle">Название:</label>
+                <label for="inputTitle">{{ trans('offers.name') }}:</label>
                 <input type="text" class="form-control" id="inputTitle" name="title" maxlength="50" value="{{ getInput('title', $offer->title) }}" required>
                 {!! textError('title') !!}
             </div>
 
             <div class="form-group{{ hasError('text') }}">
-                <label for="text">Текст:</label>
+                <label for="text">{{ trans('offers.text') }}:</label>
                 <textarea class="form-control markItUp" id="text" rows="5" name="text" required>{{ getInput('text', $offer->text) }}</textarea>
                 {!! textError('text') !!}
             </div>
 
-            <button class="btn btn-primary">Изменить</button>
+            <button class="btn btn-primary">{{ trans('offers.edit_offer') }}</button>
         </form>
     </div>
 @stop
