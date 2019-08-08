7e922d6c71f9b53445ac04e3c9fa9e7a57f0d21c
visavi <visavi.net@mail.ru>
2019-08-09 01:09:50 +0300

    локализация админки

--- resources/views/admin/spam/index.blade.php
+++ resources/views/admin/spam/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('title')
-    Жалобы
+    {{ trans('index.complains') }}
 @stop
 
 @section('breadcrumb')
@@ -9,22 +9,22 @@
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="/"><i class="fas fa-home"></i></a></li>
             <li class="breadcrumb-item"><a href="/admin">{{ trans('index.panel') }}</a></li>
-            <li class="breadcrumb-item active">Жалобы</li>
+            <li class="breadcrumb-item active">{{ trans('index.complains') }}</li>
         </ol>
     </nav>
 @stop
 
 @section('content')
     <?php $active = ($type === 'post') ? 'success' : 'light'; ?>
-    <a href="/admin/spam?type=post" class="badge badge-{{ $active }}">Форум {{ $total['post'] }}</a>
+    <a href="/admin/spam?type=post" class="badge badge-{{ $active }}">{{ trans('index.forums') }} {{ $total['post'] }}</a>
     <?php $active = ($type === 'guest') ? 'success' : 'light'; ?>
-    <a href="/admin/spam?type=guest" class="badge badge-{{ $active }}">Гостевая {{ $total['guest'] }}</a>
+    <a href="/admin/spam?type=guest" class="badge badge-{{ $active }}">{{ trans('index.guestbooks') }} {{ $total['guest'] }}</a>
     <?php $active = ($type === 'message') ? 'success' : 'light'; ?>
-    <a href="/admin/spam?type=message" class="badge badge-{{ $active }}">Приват {{ $total['message'] }}</a>
+    <a href="/admin/spam?type=message" class="badge badge-{{ $active }}">{{ trans('index.messages') }} {{ $total['message'] }}</a>
     <?php $active = ($type === 'wall') ? 'success' : 'light'; ?>
-    <a href="/admin/spam?type=wall" class="badge badge-{{ $active }}">Стена {{ $total['wall'] }}</a>
+    <a href="/admin/spam?type=wall" class="badge badge-{{ $active }}">{{ trans('index.wall_posts') }} {{ $total['wall'] }}</a>
     <?php $active = ($type === 'comment') ? 'success' : 'light'; ?>
-    <a href="/admin/spam?type=comment" class="badge badge-{{ $active }}">Комментарии {{ $total['comment'] }}</a>
+    <a href="/admin/spam?type=comment" class="badge badge-{{ $active }}">{{ trans('main.comments') }} {{ $total['comment'] }}</a>
     <br><br>
 
     @if ($records->isNotEmpty())
@@ -38,18 +38,18 @@
 
                         <div class="float-right">
                             @if (isAdmin())
-                                <a href="#" onclick="return deleteSpam(this)" data-id="{{ $data->id }}" data-token="{{ $_SESSION['token'] }}" data-toggle="tooltip" title="Удалить"><i class="fa fa-times"></i></a>
+                                <a href="#" onclick="return deleteSpam(this)" data-id="{{ $data->id }}" data-token="{{ $_SESSION['token'] }}" data-toggle="tooltip" title="{{ trans('main.delete') }}"><i class="fa fa-times"></i></a>
                             @endif
                         </div>
                     </div>
                     <div>{!! bbCode($data->relate->text) !!}</div>
                 @else
                     <div class="b">
-                        <i class="fa fa-file"></i> <b>Сообщение не найдено</b>
+                        <i class="fa fa-file"></i> <b>{{ trans('admin.spam.message_not_found') }}</b>
 
                         <div class="float-right">
                             @if (isAdmin())
-                                <a href="#" onclick="return deleteSpam(this)" data-id="{{ $data->id }}" data-token="{{ $_SESSION['token'] }}" data-toggle="tooltip" title="Удалить"><i class="fa fa-times"></i></a>
+                                <a href="#" onclick="return deleteSpam(this)" data-id="{{ $data->id }}" data-token="{{ $_SESSION['token'] }}" data-toggle="tooltip" title="{{ trans('main.delete') }}"><i class="fa fa-times"></i></a>
                             @endif
                         </div>
                     </div>
@@ -57,15 +57,15 @@
 
                 <div>
                     @if ($data->path)
-                        <a href="{{ $data->path }}">Перейти к сообщению</a><br>
+                        <a href="{{ $data->path }}">{{ trans('admin.spam.go_to_message') }}</a><br>
                     @endif
-                    Жалоба: {!! $data->user->getProfile() !!} ({{ dateFixed($data->created_at) }})
+                    {{ trans('main.sent') }}: {!! $data->user->getProfile() !!} ({{ dateFixed($data->created_at) }})
                 </div>
             </div>
         @endforeach
 
         {!! pagination($page) !!}
     @else
-        {!! showError('Жалоб еще нет!') !!}
+        {!! showError(trans('admin.spam.empty_spam')) !!}
     @endif
 @stop
