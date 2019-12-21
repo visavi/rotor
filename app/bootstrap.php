6f9492d978dcc4b299781dd8d0f57cabd191f874
visavi <visavi.net@mail.ru>
2019-12-22 02:51:40 +0300

    8.2 version

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -30,7 +30,7 @@ define('RESOURCES', BASEDIR . '/resources');
 define('STORAGE', BASEDIR . '/storage');
 define('MODULES', BASEDIR . '/modules');
 define('SITETIME', time());
-define('VERSION', '8.1');
+define('VERSION', '8.2');
 
 require_once BASEDIR . '/vendor/autoload.php';
 
