0d6a5799c4734aa1a1a6ec199b6d9f7ee0ea4ff0
visavi <visavi.net@mail.ru>
2020-10-01 00:42:36 +0300

    Поправил меню пользователя

--- public/themes/default/views/sidebar.blade.php
+++ public/themes/default/views/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Sidebar menu-->
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 <aside class="app-sidebar">
-    <ul class="app-menu cursor-pointer">
+    <ul class="app-menu user-menu">
         <li class="treeview">
         @if ($user = getUser())
             <div class="app-menu__item" data-toggle="treeview">
@@ -59,64 +59,6 @@
         @endif
         </li>
     </ul>
-
-    @if ($user = getUser())
-{{--        <div class="app-sidebar__user" data-target="#collapseLogin" data-toggle="collapse">
-            <div class="app-sidebar__user-avatar">
-                {!! $user->getAvatarImage() !!}
-            </div>
-            <div>
-                <p class="app-sidebar__user-name">{{ $user->getName() }}</p>
-                <p class="app-sidebar__user-designation">{!! $user->getStatus() !!}</p>
-            </div>
-        </div>
-
-        <div class="collapse" id="collapseLogin">
-            <ul class="app-sidebar__menu">
-                @if (isAdmin())
-                <li>
-                    <a href="/admin" rel="nofollow">
-                        <i class="app-menu__icon fas fa-wrench"></i>
-                        <span class="app-menu__label">{{ __('index.panel') }}</span>
-                    </a>
-                </li>
-                @endif
-                <li>
-                    <a href="/menu" rel="nofollow">
-                        <i class="app-menu__icon fas fa-user-cog"></i>
-                        <span class="app-menu__label ">{{ __('index.menu') }}</span>
-                    </a>
-                </li>
-            </ul>
-        </div>--}}
-    @else
-{{--        <div class="app-sidebar__user" data-target="#collapseLogin" data-toggle="collapse">
-            <div class="app-sidebar__user-avatar">
-                <img class="img-fluid rounded-circle avatar-default" src="/assets/img/images/avatar_guest.png" alt="">
-            </div>
-            <div>
-                <p class="app-sidebar__user-name">{{ __('users.enter') }}</p>
-            </div>
-        </div>
-
-        <div class="collapse" id="collapseLogin">
-            <ul class="app-sidebar__menu">
-                <li>
-                    <a href="/login{{ returnUrl() }}" rel="nofollow">
-                        <i class="app-menu__icon fas fa-sign-in-alt"></i>
-                        <span class="app-menu__label">{{ __('index.login') }}</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="/register" rel="nofollow">
-                        <i class="app-menu__icon far fa-user"></i>
-                        <span class="app-menu__label">{{ __('index.register') }}</span>
-                    </a>
-                </li>
-            </ul>
-        </div>--}}
-    @endif
-
     <ul class="app-menu">
         <li>
             <a class="app-menu__item{{ request()->is('forums*', 'topics*') ? ' active' : '' }}" href="/forums">
