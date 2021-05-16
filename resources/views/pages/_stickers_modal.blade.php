3029b2ccb4f91843387ef354d85af71cb769b779
visavi <visavi.net@mail.ru>
2021-05-17 01:45:46 +0300

    Поправил стили в стикерах

--- resources/views/pages/_stickers_modal.blade.php
+++ resources/views/pages/_stickers_modal.blade.php
@@ -1,16 +1,15 @@
 <div class="modal fade" id="stickersModal" tabindex="-1" aria-labelledby="stickersModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="stickersModalLabel">{{ __('index.stickers') }}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-
                 <div class="row mb-3">
                     @foreach($stickers as $sticker)
-                        <div class="col-md-3 col-sm-6">
-                            <a href="#" onclick="return pasteSticker(this);" class=""><img src="{{ $sticker->name }}" alt="{{ $sticker->code }}" class="img-fluid"></a>
+                        <div class="col">
+                            <a href="#" onclick="return pasteSticker(this);"><img src="{{ $sticker->name }}" alt="{{ $sticker->code }}" class="img-fluid" style="max-width: 50px;"></a>
                         </div>
                     @endforeach
                 </div>
@@ -21,7 +20,7 @@
     <script>
         pasteSticker = function (el) {
             var field = $('.markItUpEditor');
-            var paste = $(el).find('img').attr('alt');
+            var paste = $(el).find('img').attr('alt') + ' ';
 
             field.caret(paste).caret(-paste.length);
 
