6cec77fba0adf58a24799b4d32655ee2af8b4d82
visavi <visavi.net@mail.ru>
2021-06-22 23:10:33 +0300

    Раскомментировал код

--- resources/views/ads/_top.blade.php
+++ resources/views/ads/_top.blade.php
@@ -1,3 +1,3 @@
 <div class="my-1">
-    {{--{{ getAdvertPaid('top') }}--}}
+    {{ getAdvertPaid('top') }}
 </div>
--- resources/views/ads/_top_all.blade.php
+++ resources/views/ads/_top_all.blade.php
@@ -1,3 +1,3 @@
 <div class="text-center">
-    {{--{{ getAdvertPaid('top_all') }}--}}
+    {{ getAdvertPaid('top_all') }}
 </div>
