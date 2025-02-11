51ebd4bdbeb163748e5ec589ed73c33e84663aec
visavi <visavi.net@mail.ru>
2025-02-11 19:49:18 +0300

    Подключен bootstrap-tags, улучшены отступы иконок в шапке сайта

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,6 +4,17 @@ $(function () {
     translate = window['translate_' + currentLang];
 
     prettyPrint();
+    tags.init(".input-tag", {
+        allowNew: true,
+        server: "/blogs/tags/search",
+        liveServer: true,
+        clearEnd: true,
+        allowClear: true,
+        suggestionsThreshold: 2,
+        max: 5,
+        separator: [','],
+        addOnBlur: true,
+    });
 
     bootbox.addLocale('my', {
         OK : translate.buttons.ok,
