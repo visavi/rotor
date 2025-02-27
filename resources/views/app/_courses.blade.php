008e4c99dd5a4831548bf53f24545c68feff8eee
visavi <visavi.net@mail.ru>
2025-02-28 01:15:07 +0300

    Поправил анонимную функцию в курсе валют

--- resources/views/app/_courses.blade.php
+++ resources/views/app/_courses.blade.php
@@ -1,7 +1,5 @@
 <?php
-
-function trend($currency)
-{
+$trend = static function ($currency) {
     if ($currency['Value'] > $currency['Previous']) {
         return ' <i class="fas fa-caret-up text-success"></i>';
     }
@@ -10,14 +8,17 @@ function trend($currency)
     }
 
     return '';
-}
+};
 ?>
 
 @if ($courses)
     <b>{{ __('index.courses') }}</b> ({{ date('d.m.Y', strtotime($courses['Date'])) }})<br>
-    <b>{{ $courses['Valute']['USD']['Nominal'] }} {{ $courses['Valute']['USD']['CharCode'] }}</b> - {{ $courses['Valute']['USD']['Value'] }}{!! trend($courses['Valute']['USD']) !!}<br>
-    <b>{{ $courses['Valute']['EUR']['Nominal'] }} {{ $courses['Valute']['EUR']['CharCode'] }}</b> - {{ $courses['Valute']['EUR']['Value'] }}{!! trend($courses['Valute']['EUR']) !!}<br>
-    <b>{{ $courses['Valute']['UAH']['Nominal'] }} {{ $courses['Valute']['UAH']['CharCode'] }}</b> - {{ $courses['Valute']['UAH']['Value'] }}{!! trend($courses['Valute']['UAH']) !!}<br>
+    <b>{{ $courses['Valute']['USD']['Nominal'] }} {{ $courses['Valute']['USD']['CharCode'] }}</b> - {{ $courses['Valute']['USD']['Value'] }}{!! $trend($courses['Valute']['USD']) !!}
+    <br>
+    <b>{{ $courses['Valute']['EUR']['Nominal'] }} {{ $courses['Valute']['EUR']['CharCode'] }}</b> - {{ $courses['Valute']['EUR']['Value'] }}{!! $trend($courses['Valute']['EUR']) !!}
+    <br>
+    <b>{{ $courses['Valute']['UAH']['Nominal'] }} {{ $courses['Valute']['UAH']['CharCode'] }}</b> - {{ $courses['Valute']['UAH']['Value'] }}{!! $trend($courses['Valute']['UAH']) !!}
+    <br>
 
 @else
     {{ showError(__('index.courses_error')) }}
