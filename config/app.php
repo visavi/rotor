59f76126019fae04b145706e0eec742156dfaa05
visavi <visavi.net@mail.ru>
2025-03-23 04:46:08 +0300

    fix config/app.php

--- config/app.php
+++ config/app.php
@@ -158,48 +158,4 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store'  => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Autoloaded Service Providers
-    |--------------------------------------------------------------------------
-    |
-    | The service providers listed here will be automatically loaded on the
-    | request to your application. Feel free to add your own services to
-    | this array to grant expanded functionality to your applications.
-    |
-    */
-
-    //'providers' => ServiceProvider::defaultProviders()->merge([
-    /*
-         * Package Service Providers...
-         */
-
-    /*
-         * Application Service Providers...
-         */
-    /*        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-
-        App\Providers\ImageServiceProvider::class,
-        App\Providers\ModuleServiceProvider::class,
-    ])->toArray(),*/
-
-    /*
-    |--------------------------------------------------------------------------
-    | Class Aliases
-    |--------------------------------------------------------------------------
-    |
-    | This array of class aliases will be registered when this application
-    | is started. However, feel free to register as many as you wish as
-    | the aliases are "lazy" loaded so they don't hinder performance.
-    |
-    */
-
-    /*    'aliases' => Facade::defaultAliases()->merge([
-        // ...
-    ])->toArray(),*/
 ];
