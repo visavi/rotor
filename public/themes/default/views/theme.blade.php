ab7e0254479fdf59a092bf0782896e4e2069d24a
visavi <visavi.net@mail.ru>
2025-06-04 11:57:11 +0300

    Добавлена каноническая ссылка указывающая на главную страницу без учета get параметров

--- public/themes/default/views/theme.blade.php
+++ public/themes/default/views/theme.blade.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') - {{ setting('title') }}</title>
+    <link rel="canonical" href="{{ request()->url() }}">
     <link href="/favicon.ico" rel="icon">
     <link href="/assets/img/images/icon.png" rel="image_src">
     <link href="/news/rss" title="RSS News" type="application/rss+xml" rel="alternate">
