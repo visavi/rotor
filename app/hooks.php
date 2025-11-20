b7a30a95d8263ec3d9ac7f9f382cd17be9d5e94e
visavi <visavi.net@mail.ru>
2025-11-21 02:33:25 +0300

    Добавил правильные примеры хуков

--- app/hooks.php
+++ app/hooks.php
@@ -6,7 +6,12 @@ use App\Classes\Hook;
  * Хуки смотрите в исходном коде сайта, html комментарии начинаются с символа @, к примеру <!--@head-->
  */
 
-// Пример хука
+// Пример css хука
 /* Hook::add('head', function ($content) {
-    return $content . '<link rel="stylesheet" href="style.css">' . PHP_EOL;
-}); */
+    return $content . '<link rel="stylesheet" href="/assets/styles.css">' . PHP_EOL;
+});*/
+
+// Пример js хука
+/*Hook::add('footer', function ($content) {
+    return $content . '<script type="module" src="/assets/scripts.js"></script>' . PHP_EOL;
+});*/
