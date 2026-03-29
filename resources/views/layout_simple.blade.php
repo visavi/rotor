494dbe9a5210be3befcbdb32c0e1dad550f89bfe
visavi <visavi.net@mail.ru>
2026-03-29 17:42:56 +0300

    fix layout simple

--- resources/views/layout_simple.blade.php
+++ resources/views/layout_simple.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title') - {{ setting('title') }}</title>
-    @vite('public/assets/themes/default/sass/app.scss')
+    @vite('resources/themes/default/sass/app.scss')
     @stack('styles')
 </head>
 <body>
@@ -15,7 +15,7 @@
         @yield('content')
     </div>
 
-    @vite('public/assets/themes/default/js/app.js')
+    @vite('resources/themes/default/js/app.js')
     @stack('scripts')
 </body>
 </html>
