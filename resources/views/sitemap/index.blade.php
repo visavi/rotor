cea6967c1bf8f7b122e6b9921a467b85990bae7c
visavi <visavi.net@mail.ru>
2026-05-04 12:20:15 +0300

    fix sitemap.xml

--- resources/views/sitemap/index.blade.php
+++ resources/views/sitemap/index.blade.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
+{!! '<?xml version="1.0" encoding="utf-8"?>' !!}
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 @foreach ($locs as $loc)
     <sitemap>
