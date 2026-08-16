1b47ea54dc108bc95a7c1a41a5e9aae48ec4b3bf
visavi <visavi.net@mail.ru>
2026-08-16 21:14:37 +0300

    Поправил тесты

--- tests/ModuleTestCase.php
+++ tests/ModuleTestCase.php
@@ -6,6 +6,7 @@ use App\Http\Middleware\CheckInstallSite;
 use App\Providers\ModuleServiceProvider;
 use App\Support\Hook;
 use App\Support\Registry;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Str;
 
@@ -54,6 +55,24 @@ abstract class ModuleTestCase extends TestCase
         $hooksFile = base_path("modules/{$name}/hooks.php");
         if (file_exists($hooksFile)) {
             require $hooksFile;
+        }
+
+        // Своя прослойка модуля: алиасы и группа web, как в провайдере
+        $middlewareFile = base_path("modules/{$name}/middleware.php");
+        if (file_exists($middlewareFile)) {
+            $middleware = include $middlewareFile;
+
+            foreach ($middleware['aliases'] ?? [] as $alias => $class) {
+                $this->app['router']->aliasMiddleware($alias, $class);
+            }
+
+            // Группы приходят из ядра при его инициализации и затирают запись
+            // напрямую в роутер, поэтому прослойку добавляем самому ядру
+            $kernel = $this->app->make(Kernel::class);
+
+            foreach ($middleware['web'] ?? [] as $class) {
+                $kernel->appendMiddlewareToGroup('web', $class);
+            }
         }
 
         $routesFile = base_path("modules/{$name}/routes.php");
