a58d79b0b2cede67c348d693b80d791e9af53f19
visavi <visavi.net@mail.ru>
2026-08-30 03:27:45 +0300

    Поправил стили темы simple

--- resources/views/themes/simple/footer.blade.php
+++ resources/views/themes/simple/footer.blade.php
@@ -2,15 +2,38 @@
 <footer class="site-footer">
     <div class="wrap">
         @hook('footerStart')
-        <div class="site-footer__links">
-            <a href="{{ route('pages') }}">{{ __('index.pages') }}</a>
-            <span><a href="{{ route('users.index') }}">{{ __('index.users') }}</a> <span class="badge bg-adaptive">{{ statsUsers() }}</span></span>
-            <a href="{{ route('mails.index') }}">{{ __('index.mails') }}</a>
-            <a href="{{ route('search') }}">{{ __('index.search') }}</a>
-            @hook('footerColumnStart')
-            @hook('footerColumnMiddle')
-            @hook('footerColumnEnd')
+
+        <div class="site-footer__cols">
+            <div>
+                <h6>{{ __('index.pages') }}</h6>
+                <ul>
+                    <li><a href="{{ route('pages') }}">{{ __('index.pages') }}</a></li>
+                    @hook('footerColumnStart')
+                </ul>
+            </div>
+
+            <div>
+                <h6>{{ __('index.users') }}</h6>
+                <ul>
+                    <li>
+                        <a href="{{ route('users.index') }}">{{ __('index.users') }}</a>
+                        <span class="badge bg-adaptive">{{ statsUsers() }}</span>
+                    </li>
+                    @hook('footerColumnMiddle')
+                </ul>
+            </div>
+
+            <div>
+                <h6>{{ __('index.mails') }}</h6>
+                <ul>
+                    <li><a href="{{ route('mails.index') }}">{{ __('index.mails') }}</a></li>
+                    <li><a href="{{ route('search') }}">{{ __('index.search') }}</a></li>
+                    @hook('footerColumnEnd')
+                </ul>
+            </div>
         </div>
+
+        <div class="site-footer__counter">{{ showCounter() }}</div>
 
         <div class="site-footer__bottom">
             <span>{{ setting('copy') }}</span>
@@ -20,9 +43,9 @@
                     <img src="/assets/flags/{{ app()->getLocale() }}.svg" alt="" width="18" class="flag" onerror="this.remove()">
                     {{ __('main.lang') }}
                 </a>
-                {{ showCounter() }}
             </span>
         </div>
+
         @hook('footerEnd')
     </div>
     {{ performance() }}
