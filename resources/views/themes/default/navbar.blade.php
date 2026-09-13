ab9fad845b90c0527fadb693b93c6b29a1d770ba
visavi <visavi.net@mail.ru>
2026-09-13 18:25:50 +0300

    Поправил стили в default и в диалогах

--- resources/views/themes/default/navbar.blade.php
+++ resources/views/themes/default/navbar.blade.php
@@ -50,7 +50,7 @@
 
             @if ($user->isActive())
                 <li class="dropdown js-messages-block">
-                    <a class="app-nav__item" href="#" data-bs-toggle="dropdown" aria-label="Show notifications">
+                    <a class="app-nav__item" href="#" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Show notifications">
                         <i class="far fa-envelope fa-lg"></i>
                         <span class="badge bg-notify js-message-count">{{ $user->newprivat ?: '' }}</span>
                     </a>
@@ -64,7 +64,7 @@
 
             <!-- User Menu-->
             <li class="dropdown">
-                <a class="app-nav__item" href="#" data-bs-toggle="dropdown" aria-label="Open Profile Menu">
+                <a class="app-nav__item" href="#" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Open Profile Menu">
                     <i class="far fa-user fa-lg"></i>
                 </a>
                 <ul class="app-notification dropdown-menu dropdown-menu-end">
